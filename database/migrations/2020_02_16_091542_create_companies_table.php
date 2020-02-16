12057218efb479815ee232b11df707c74add60ba
Joshua Bakasa <baksajoshua09@gmail.com>
2020-02-16 13:57:31 +0300

    Creation of the setup tables and models

---
+++ database/migrations/2020_02_16_091542_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Companies', function (Blueprint $table) {
+            $table->string('Company_Code', 10)->primary();
+            $table->string('Company_Name');
+            $table->string('Local_Currency_Code', 10);
+            $table->string('Country_Code', 10);
+
+            $table->foreign('Country_Code')->references('Country_Code')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ database/migrations/2020_02_16_092045_create_currencies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->string('Currency_Code', 10)->primary();
+            $table->string('Country_Code', 10);
+            $table->decimal('Exchange_Rate');
+
+            $table->foreign('Country_Code')->references('Country_Code')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
