823ff4a46cd52d8b4cd96dedc99279892652fefd
Joshua Bakasa <baksajoshua09@gmail.com>
2020-02-16 16:21:43 +0300

    Begin test on data import to MySQL

---
+++ app/Random.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Imports\BlowplastImport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class Random extends Model
+{
+    public static function import()
+    {
+    	Excel::import(new BlowplastImport, 'blowplast.xlsx');
+    }
+}
