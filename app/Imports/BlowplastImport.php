17274dfcbf4a65067d5447f41b1b633b3dcfb988
Joshua Bakasa <baksajoshua09@gmail.com>
2020-03-18 18:53:28 +0300

    Importing the GL Accounts and entries

--- app/Imports/BlowplastImport.php
+++ app/Imports/BlowplastImport.php
@@ -12,14 +12,15 @@ class BlowplastImport implements WithMultipleSheets
     public function sheets(): array
     {
         return [
-            'Country' => new CountrySheetImport(),
-            'Company' => new CompanySheetImport(),
-            'Currency' => new CurrencySheetImport(),
-            'Customers' => new CustomerSheetImport(),
-            'Inventory' => new InventorySheetImport(),
+            // 'Country' => new CountrySheetImport(),
+            // 'Company' => new CompanySheetImport(),
+            // 'Currency' => new CurrencySheetImport(),
+            // 'Customers' => new CustomerSheetImport(),
+            // 'Inventory' => new InventorySheetImport(),
+            'GL Accounts' =>  new GLAccountsSheetImport(),
+            'GL Entries' => new GLEntriesSheetImport(),
             // 'sales invoicecredit memo header' => new SalesInvoiceCreditMemoHeaderSheetImport(),
             // 'sales line' => new SalesInvoiceCreditMemoLineSheetImport(),
-            // 'Gl Accounts' => new GLAccountsSheetImport(),
         ];
     }
 }
---
+++ app/Imports/GLEntriesSheetImport.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Imports;
+
+use App\GLEntries;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class GLEntriesSheetImport implements ToModel, WithHeadingRow
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new GLEntries([
+            "Entry_No" => $row["entry_no"],
+            "GL_Account_No" => $row["gl_account_number"],
+            "Balancing_GL_Account_No" => $row["balancing_gl_account_no"],
+            "Amounts" => $row["amount"],
+            "Currency_Code" => $row["currency"],
+            "Posting_Date" => $row["posting_date"],
+            "Document_No" => $row["document_number"],
+            "Document_Type" => $row["document_type"],
+            "Description" => $row["description"],
+            "Company_Code" => $row["company_code"],
+        ]);
+    }
+}
