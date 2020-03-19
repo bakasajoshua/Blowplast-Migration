03550cc6fb748ff3cd95147957de46f5eee85954
Joshua Bakasa <baksajoshua09@gmail.com>
2020-03-19 09:36:38 +0300

    GL Accounts inserted

--- app/Imports/BlowplastImport.php
+++ app/Imports/BlowplastImport.php
@@ -12,17 +12,17 @@ class BlowplastImport implements WithMultipleSheets
     public function sheets(): array
     {
         return [
-            'Country' => new CountrySheetImport(),
-            'Company' => new CompanySheetImport(),
-            'Currency' => new CurrencySheetImport(),
-            'Customers' => new CustomerSheetImport(),
-            'Inventory' => new InventorySheetImport(),
-            'sales invoicecredit memo header' => new SalesInvoiceCreditMemoHeaderSheetImport(),
-            'sales line' => new SalesInvoiceCreditMemoLineSheetImport(),
-            'Customer Ledger Entries' => new CustomerLedgerEntriesSheetImport(),
+            // 'Country' => new CountrySheetImport(),
+            // 'Company' => new CompanySheetImport(),
+            // 'Currency' => new CurrencySheetImport(),
+            // 'Customers' => new CustomerSheetImport(),
+            // 'Inventory' => new InventorySheetImport(),
+            // 'sales invoicecredit memo header' => new SalesInvoiceCreditMemoHeaderSheetImport(),
+            // 'sales line' => new SalesInvoiceCreditMemoLineSheetImport(),
+            // 'Customer Ledger Entries' => new CustomerLedgerEntriesSheetImport(),
+            'GL Accounts' =>  new GLAccountsSheetImport(),
 
             
-            // 'GL Accounts' =>  new GLAccountsSheetImport(),
             // 'GL Entries' => new GLEntriesSheetImport(),
             
         ];
