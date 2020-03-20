ca5b040c0a53f1778aa19fee7759d9f0f0809464
Joshua Bakasa <baksajoshua09@gmail.com>
2020-03-20 12:49:03 +0300

    Added the time lookup tables

---
+++ database/migrations/2020_03_20_093310_create_month_of_years_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMonthOfYearsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('LU_Month_Of_Year', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->srting('month_description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('LU_Month_Of_Year');
+    }
+}
---
+++ database/migrations/2020_03_20_093542_create_days_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('LU_Day', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('day');
+            $table->integer('week');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('LU_Day');
+    }
+}
