97aceaf61b5a8448f0b260c84cb31c71eed4c2cd
Joshua Bakasa <baksajoshua09@gmail.com>
2020-03-23 23:24:11 +0300

    Update names

--- database/migrations/2020_03_23_231342_Alter_tables.php
+++ database/migrations/2020_03_23_231342_Alter_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Alter_tables extends Migration
+class AlterTables extends Migration
 {
     /**
      * Run the migrations.
