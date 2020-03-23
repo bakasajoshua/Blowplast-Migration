5a6c3930ddf456636168b80bdd87a88c44246238
Joshua Bakasa <baksajoshua09@gmail.com>
2020-03-23 21:35:03 +0300

    Addition of the posting date columns

--- app/Random.php
+++ app/Random.php
@@ -5,13 +5,15 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\Imports\BlowplastImport;
 use App\Imports\GLEntriesSheetImport;
+use App\Imports\GLEntriesUpdateSheetImport;
 use Maatwebsite\Excel\Facades\Excel;
 
 class Random extends Model
 {
     public static function import()
     {
-    	Excel::import(new BlowplastImport, public_path('import/blowplast.xlsx'));
-    	Excel::import(new GLEntriesSheetImport, public_path('import/glentries.csv'));
+    	// Excel::import(new BlowplastImport, public_path('import/blowplast.xlsx'));
+    	// Excel::import(new GLEntriesSheetImport, public_path('import/glentries.csv'));
+    	Excel::import(new GLEntriesUpdateSheetImport, public_path('import/postingdates2.xlsx'));
     }
 }
