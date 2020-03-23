4ffadf92602a2b7346949ba50875ba77fad17008
Joshua Bakasa <baksajoshua09@gmail.com>
2020-03-23 23:25:21 +0300

    Update names

--- database/migrations/2020_03_23_231342_Alter_tables.php
+++ database/migrations/2020_03_23_231342_Alter_tables.php
@@ -13,25 +13,25 @@ class AlterTables extends Migration
      */
     public function up()
     {
-        Schema::create('Customer Ledger Entries', function (Blueprint $table) {
+        Schema::table('Customer Ledger Entries', function (Blueprint $table) {
             $table->renameColumn('Entry_No', 'CU_Leg_Entry_No');
         });
 
-        Schema::create('GL Entries', function (Blueprint $table) {            
+        Schema::table('GL Entries', function (Blueprint $table) {            
             $table->renameColumn('Entry_No', 'GL_Leg_Entry_No');            
             $table->renameColumn('Posting_Date', 'GL_Leg_Posting_Date');
             $table->renameColumn('Document_No', 'GL_LegDocument_No');
             $table->renameColumn('Document_Type', 'GL_LegDocument_Type');
         });
 
-        Schema::create('Sales Invoice Credit Memo Headers', function (Blueprint $table) {
+        Schema::table('Sales Invoice Credit Memo Headers', function (Blueprint $table) {
             $table->renameColumn('Document_No', 'SI_Document_No');
             $table->renameColumn('Posting_Date', 'SI_Posting_Date');
             $table->renameColumn('Due_Date', 'SI_Due_Date');
             $table->renameColumn('Order_Date', 'SI_Order_Date');
         });
 
-        Schema::create('Sales Invoice Credit Memo Lines', function (Blueprint $table) {
+        Schema::table('Sales Invoice Credit Memo Lines', function (Blueprint $table) {
             $table->renameColumn('Line_No', 'SI_Li_Line_No');
             $table->renameColumn('Document_No', 'SI_Li_Document_No');
             $table->renameColumn('Type', 'SI_Li_Type');
