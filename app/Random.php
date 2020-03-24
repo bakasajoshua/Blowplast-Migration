6da61a3cc0c18eb501df9bf994280bd70fdbb2c6
Joshua Bakasa <baksajoshua09@gmail.com>
2020-03-24 07:25:18 +0300

    Refreshing the GL entries data

--- app/Random.php
+++ app/Random.php
@@ -12,20 +12,37 @@ class Random extends Model
 {
     public static function import()
     {
-    	// echo "==> Truncating tables\n";
-    	// SalesInvoiceCreditMemoHeader::truncate();
-    	// SalesInvoiceCreditMemoLine::truncate();
-    	// CustomerLedgerEntry::truncate();
-    	// GLAccounts::truncate();
-    	// GLEntries::truncate();
+    	
 
-    	// echo "==> Inserting the general data\n";
-    	// Excel::import(new BlowplastImport, public_path('import/blowplast.xlsx'));
-    	// ini_set("memory_limit", "-1");
-    	echo "==> Inserting the GL entries data\n";
-    	Excel::import(new GLEntriesSheetImport, public_path('import/glentries.csv'));
-    	// Excel::import(new GLEntriesUpdateSheetImport, public_path('import/postingdates2.xlsx'));
+    	// Open a try/catch block
+		try {
+		    // Begin a transaction
+		    DB::beginTransaction();
 
-    	echo "==> Data import complete\n";
+		    // echo "==> Truncating tables\n";
+	    	// SalesInvoiceCreditMemoHeader::truncate();
+	    	// SalesInvoiceCreditMemoLine::truncate();
+	    	// CustomerLedgerEntry::truncate();
+	    	// GLAccounts::truncate();
+	    	// GLEntries::truncate();
+
+	    	// echo "==> Inserting the general data\n";
+	    	// Excel::import(new BlowplastImport, public_path('import/blowplast.xlsx'));
+	    	// ini_set("memory_limit", "-1");
+	    	echo "==> Inserting the GL entries data\n";
+	    	Excel::import(new GLEntriesSheetImport, public_path('import/glentries.csv'));
+	    	// Excel::import(new GLEntriesUpdateSheetImport, public_path('import/postingdates2.xlsx'));
+
+	    	echo "==> Data import complete\n";
+
+		    // Commit the transaction
+		    DB::commit();
+		} catch (\Exception $e) {
+		    // An error occured; cancel the transaction...
+		    DB::rollback();
+
+		    // and throw the error again.
+		    throw $e;
+		}
     }
 }
