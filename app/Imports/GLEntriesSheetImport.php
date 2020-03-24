04d1eee62a82bcd917548919fab073c3a3ba0027
Joshua Bakasa <baksajoshua09@gmail.com>
2020-03-24 06:04:19 +0300

    Refreshing the GL entries data

--- app/Imports/GLEntriesSheetImport.php
+++ app/Imports/GLEntriesSheetImport.php
@@ -6,7 +6,7 @@ use App\GLEntries;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
-use Carbon\Carbon;
+// use Carbon\Carbon;
 
 class GLEntriesSheetImport implements ToModel, WithHeadingRow, WithChunkReading
 {
@@ -17,15 +17,15 @@ class GLEntriesSheetImport implements ToModel, WithHeadingRow, WithChunkReading
     */
     public function model(array $row)
     {
-        dd($row);
-        $posting_date = Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['posting_date']))->format('Y-m-d');
+        // dd($row);
+        // $posting_date = Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['posting_date']))->format('Y-m-d');
         return new GLEntries([
             // "Entry_No" => $row["entry_no"],
             "GL_Account_No" => $row["gl_account_no"],
             "Balancing_GL_Account_No" => $row["balancing_gl_account_no"],
             "Amounts" => $row["amounts"],
             "Currency_Code" => $row["currency_code"],
-            "GL_Posting_Date" => $posting_date ?? NULL,
+            "GL_Posting_Date" => $row['posting_date'],
             "GL_Document_No" => $row["document_no"],
             "GL_Document_Type" => $row["document_type"],
             "Description" => $row["description"],
