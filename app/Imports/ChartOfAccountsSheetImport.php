81c572d173ae16799d3ff435b7ba6e3a01c79270
Joshua Bakasa <baksajoshua09@gmail.com>
2020-04-06 15:04:56 +0300

    Bug fix

--- app/Imports/ChartOfAccountsSheetImport.php
+++ app/Imports/ChartOfAccountsSheetImport.php
@@ -13,10 +13,12 @@ class ChartOfAccountsSheetImport implements ToModel, WithHeadingRow
     */
     public function model(array $row)
     {
-        return new ChartOfAccounts([
-            "Chart_of_Account_Group" => $row["lu_gl_accounts_level_1"],
-            "Chart_of_Account_Group_Name" => $row['chart_of_account_group'],
-            "LU_GL_Accounts_Level_1" => $row['chart_of_account_group_name'],
-        ]);
+    	// dd($row);
+    	if (null !== $row["lu_gl_accounts_level_1"])
+	        return new ChartOfAccounts([
+	            "Chart_of_Account_Group" => $row['chart_of_account_group'],
+	            "Chart_of_Account_Group_Name" => $row['chart_of_account_group_name'],
+	            "LU_GL_Accounts_Level_1" => $row["lu_gl_accounts_level_1"],
+	        ]);
     }
 }
