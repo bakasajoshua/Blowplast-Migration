5c3b23c97cddf7b7e0077029525bc8a8e2abf3bc
Joshua Bakasa <baksajoshua09@gmail.com>
2020-04-16 23:43:17 +0300

    Added the proper columns and remove memory limits

--- app/GLEntries.php
+++ app/GLEntries.php
@@ -19,20 +19,21 @@ class GLEntries extends BaseModel
     private $functionCall = "GetGLEntries";
 
     private $endpointColumns = [
-        'GL_Entry_No' => 'Entry_x0020_No'
-        'GL_Account_No' => 'GL_x0020_Account_x0020_Number'
-        'Balancing_GL_Account_No' => 'Balancing_x0020_GL_x0020_Account_x0020_No'
-        'Amounts' => 'Amount'
-        'GL_Posting_Date' => 'Posting_x0020_Date'
-        'GL_Document_No' => 'Document_x0020_Number'
-        'GL_Document_Type' => 'Document_x0020_Type'
-        'Description' => 'Description'
-        'Company_Code' => 'Company_x0020_Code'
+        'GL_Entry_No' => 'Entry_x0020_No',
+        'GL_Account_No' => 'GL_x0020_Account_x0020_Number',
+        'Balancing_GL_Account_No' => 'Balancing_x0020_GL_x0020_Account_x0020_No',
+        'Amounts' => 'Amount',
+        'GL_Posting_Date' => 'Posting_x0020_Date',
+        'GL_Document_No' => 'Document_x0020_Number',
+        'GL_Document_Type' => 'Document_x0020_Type',
+        'Description' => 'Description',
+        'Company_Code' => 'Company_x0020_Code',
     ];
     private $chunkQty = 100;
 
     public function synchEntries()
     {
+        ini_set("memory_limit", "-1");
         $chunks = $this->synch($this->functionCall, $this->endpointColumns)->chunk($this->chunkQty);
         foreach ($chunks as $key => $data) {
             GLEntries::insert($data->toArray());
