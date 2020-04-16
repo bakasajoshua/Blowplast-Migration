1c1a5362f198e25c93114568928cef924f507b0f
Joshua Bakasa <baksajoshua09@gmail.com>
2020-04-16 13:43:26 +0300

    XML reader working

--- app/Customer.php
+++ app/Customer.php
@@ -18,4 +18,27 @@ class Customer extends Model
     {
         return SoapCli::call($this->functionCall);
     }
+
+    public function getData()
+    {
+    	$z = new \XMLReader;
+		$z->open(public_path('data/customer.xml'));
+		$doc = new \DOMDocument;
+		$data = [];
+		$count = 0;
+		// move to the first <product /> node
+		while ($z->read())
+		{
+			while ($z->name == "Table") {
+				$node = simplexml_import_dom($doc->importNode($z->expand(), true));
+				$data[] = [
+						'Customer_No' => collect((array)$node->Customer_x0020_No)->first(),
+						'Customer_Name' => collect((array)$node->Customer_x0020_Name)->first() ?? '',
+						'Company_Code' => collect((array)$node->Company_x0020_Code)->first() ?? 'BUL'
+					];
+		    	$z->next('Table');
+			}
+		}
+		dd(collect($data));
+    }
 }
