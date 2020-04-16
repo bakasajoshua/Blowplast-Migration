dcbb19811014fd3acbf8748361a1d981a1ed2a09
Joshua Bakasa <Joshua.Bakasa@bplgroup.local>
2020-04-16 20:58:43 +0300

    Bringing in the GL Entries

--- app/GLEntries.php
+++ app/GLEntries.php
@@ -18,6 +18,18 @@ class GLEntries extends Model
 
     private $functionCall = "GetGLEntries";
 
+    private $endpointColumns = [
+        'GL_Entry_No' => 'Entry_x0020_No'
+        'GL_Account_No' => 'GL_x0020_Account_x0020_Number'
+        'Balancing_GL_Account_No' => 'Balancing_x0020_GL_x0020_Account_x0020_No'
+        'Amounts' => 'Amount'
+        'GL_Posting_Date' => 'Posting_x0020_Date'
+        'GL_Document_No' => 'Document_x0020_Number'
+        'GL_Document_Type' => 'Document_x0020_Type'
+        'Description' => 'Description'
+        'Company_Code' => 'Company_x0020_Code'
+    ]
+
 	public static function boot()
     {
         parent::boot();
