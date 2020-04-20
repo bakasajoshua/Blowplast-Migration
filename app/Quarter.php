39d3546d93dd112bad858a9e258a6edbd91b0dac
Joshua Bakasa <baksajoshua09@gmail.com>
2020-04-20 10:30:04 +0300

    Addind the necessary attributes to the time models

--- app/Quarter.php
+++ app/Quarter.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quarter extends Model
 {
-    //
+    protected $table = 'LU_Quarter';
+
+    protected $guarded = [];
+
+    public $timestamps = false;
 }
