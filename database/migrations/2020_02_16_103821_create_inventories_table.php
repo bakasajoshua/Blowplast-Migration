4580e1f251907e238b47cb150c5b09768b848415
Joshua Bakasa <baksajoshua09@gmail.com>
2020-05-13 14:54:39 +0300

    Returning migrations files to the root migrations folder

---
+++ database/migrations/2020_02_16_103821_create_inventories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Item Master', function (Blueprint $table) {
+            $table->string('Item_No', 50)->primary();
+            $table->text('Item_Description')->nullable();
+            $table->string('Company_Code')->nullable();
+            $table->string('Dimension1')->nullable();
+            $table->string('Dimension2')->nullable();
+
+            // $table->foreign('Company_Code')->references('Company_Code')->on('Companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Item Master');
+        Schema::dropIfExists('Inventories');
+    }
+}
---
+++ database/migrations/2020_04_06_110159_create_account_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('GL_Accounts_Level_1', function (Blueprint $table) {
+            $table->increments('Level_1_ID');
+            $table->string('Level_1_Description', 100);
+            $table->string('bs_is')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('GL_Accounts_Level_1');
+    }
+}
