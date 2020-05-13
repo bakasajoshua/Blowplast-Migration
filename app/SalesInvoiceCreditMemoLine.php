78e06ae5f9119a06934b9c05d07bfbf14ff6890e
Joshua Bakasa <joshua.bakasa@dataposit.co.ke>
2020-05-14 01:14:01 +0300

    Updates to make sales lines to import successfully

--- app/SalesInvoiceCreditMemoLine.php
+++ app/SalesInvoiceCreditMemoLine.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SalesInvoiceCreditMemoLine extends Model
+class SalesInvoiceCreditMemoLine extends BaseModel
 {
     protected $table = 'Sales Invoice Credit Memo Lines';
 
