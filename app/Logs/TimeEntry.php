8b1e9242c8fc44be4f70074a83f16433fa317c2e
Joshua Bakasa <joshua.bakasa@dataposit.co.ke>
2020-08-03 22:07:12 +0300

    Remove have the time entries in all the DBs

--- app/Logs/TimeEntry.php
+++ app/Logs/TimeEntry.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class TimeEntry extends Model
 {
-    protected $connection = 'testdb';
+    // protected $connection = 'testdb';
 
     protected $guarded = ['id'];
 }
