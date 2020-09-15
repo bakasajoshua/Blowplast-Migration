34e8659a6aba2361f1036535414c0b3171fb222d
Joshua Bakasa <joshua.bakasa@dataposit.co.ke>
2020-09-15 17:30:14 +0300

    Update

--- app/InventoryBudget.php
+++ app/InventoryBudget.php
@@ -23,12 +23,14 @@ class InventoryBudget extends Model
     public static function harmonizeItemNoKE()
     {
     	$budgetItems = InventoryBudget::where('Company_Code', 'BPL')->get()->unique('Item_Description');
-    	foreach ($budgetItems as $key => $budgetItem) {
-    		$item = Inventory::where('Item_Description', $budgetItem->Item_Description)
-    					->where('Company_Code', $budgetItem->Company_Code)->get();
-    		$budgetItem->Item_No = $item->first()->Item_No;
-    		$budgetItem->save();
-    	}
-    	return true;
+        foreach ($budgetItems as $key => $budgetItem) {
+            $item = Inventory::where('Item_Description', $budgetItem->Item_Description)
+                        ->where('Company_Code', $budgetItem->Company_Code)->get();
+            if (!$item->isEmpty()){
+                $budgetItem->Item_No = $item->first()->Item_No;
+                $budgetItem->save();
+            }
+        }
+        return true;
     }
 }
