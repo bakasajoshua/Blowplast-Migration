e0355dd67a75062f3eaff32997486b93a25b45d7
Joshua Bakasa <joshua.bakasa@dataposit.co.ke>
2020-11-25 09:33:11 +0300

    Addition of the credit notes

--- app/Temps/TempReceivable.php
+++ app/Temps/TempReceivable.php
@@ -21,22 +21,35 @@ class TempReceivable extends Model
     
     public static function insertData($verbose=false)
     {
+        ini_set("memory_limit", "-1");
         if ($verbose)
             echo "==> Start pulling KE Receivables Data " . date('Y-m-d H:i:s') . "\n";
         $data = DB::connection('oracle')->select('select * from fin.fin_ar_vw');
         if ($verbose){
             echo "==> Finished pulling KE Data " . date('Y-m-d H:i:s') . "\n";
-            echo "==> Inserting Temp KE Data " . date('Y-m-d H:i:s') . "\n";
         }
-        foreach ($data as $key => $value) {
-            $model = new TempReceivable;
-            $model->fill((array)$value);
-            $model->save();
-        }
+
         if ($verbose)
-            echo "==> Finished Inserting KE Receivables Data into the WH " . date('Y-m-d H:i:s') . "\n";    	
+            echo "==> Truncate the Receivables temp table " . date('Y-m-d H:i:s') . "\n";
+        TempReceivable::truncate();
+
+        if ($verbose)
+            echo "==> Inserting Temp KE Data " . date('Y-m-d H:i:s') . "\n";
+        $importData = [];
+        foreach ($data as $key => $value) {
+            $importData[] = (array)$value;
+        }
+        $chunks = collect($importData)->chunk(100);
+        foreach ($chunks as $key => $chunk) {
+            TempReceivable::insert($chunk->toArray());
+        }
+
+        if ($verbose)
+            echo "==> Finished Inserting KE Receivables Data into the WH " . date('Y-m-d H:i:s') . "\n";
+
+        self::manuals($verbose);
         
-        return $model;
+        return true;
     }
 
     public static function manuals($verbose=false)
@@ -44,9 +57,23 @@ class TempReceivable extends Model
         $model = new TempReceivable;
         $headerdata = $lilnedata = [];
         if ($verbose)
+            echo "==> Removing the existing credit notes from the WH " . date('Y-m-d H:i:s') . "\n";
+        $credheaders = SalesInvoiceCreditMemoHeader::where('Type', 'Credit Note')->where('Company_Code', 'BPL')->get();
+        foreach ($credheaders as $key => $value) {
+            $value->delete();
+        }
+        $credlines = SalesInvoiceCreditMemoLine::where('Type', 'Credit Note')->where('Company_Code', 'BPL')->get();
+        foreach ($credlines as $key => $value) {
+            $value->delete();
+        }
+        if ($verbose)
+            echo "==> Finished removing the existing credit notes from the WH " . date('Y-m-d H:i:s') . "\n";
+
+        if ($verbose)
             echo "==> Getting the credit notes and formatting them " . date('Y-m-d H:i:s') . "\n";
         foreach ($model->no_prefixes as $prefixkey => $prefix) {
             $invoices = TempReceivable::where('voucher_number', 'like', '%'.$prefix.'%')->get();
+            echo "==> Credit Notes found {$invoices->count()} \n";
             foreach ($invoices as $invoicekey => $invoice) {
                 $customer = Customer::where('Customer_Name', $invoice->supplier_name)->get();
                 if (!$customer->isEmpty()) {
